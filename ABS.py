0e405371cd7c12770a13d94aa554f497d65f525c
kit1ket <99211435+kit1ket@users.noreply.github.com>
2023-04-06 15:09:16 +0300

    Add files via upload

--- ABS.py
+++ ABS.py
@@ -1,5 +1,6 @@
 import math
 
+
 def sign(x):
     if x < 0:
         return -1
@@ -16,106 +17,100 @@ def norm(x, bounds):
     else:
         return x
 
-dt = 0.5
-on = 1
 
-# Контроллер АБС
-# s - коэффициент скольжения
-# v - скорость автомобиля, м/с
-# next_k - возвращает коэффициент гидравлики тормозов
-# is_active - возвращает True, если АБС функционирует
-# Возвращает тормозной момент, Н*м
-#максимальное значение кэф трения достигается только при проскальзывании в 20% поэтому 0.2 
-def controller(prev_Tb, s, v, next_k, is_active, Tb_max, dt = dt):
-    if not is_active(v):
-        s = 0
-    s_sign = sign(0.2 - s)
-    Tb = prev_Tb + s_sign * next_k() / (1 + dt * s)
-    return norm(Tb, (0, Tb_max))
+#--------------------------------------------------------------------------------------------
 
-# Считает коэффициент скольжения
-# vw - предполагаемая угловая скорость колеса автомобиля, рад/с
-# next_ww - считает фактическую угловую скорость колеса, рад/с
-# eps - какое-то маленькое число, чтобы случайно не поделить на ноль
-def slip(vw, next_ww, eps):
-    return norm((1 - (next_ww() / (vw + (vw == 0) * eps))), (0, 1))
-
-# Считает угловую скорость колеса, рад/с
-# Ff - сила трения, Н
-# Tb - крутящий момент торможения, Н*м
-# dt - время моделирования, c
-def wheel(prev_ww, Tb, Ff, rw, Jw):
-    return norm(prev_ww - (Tb - rw * Ff) * Jw, (0, 1000))
-
-# Считает 
-# - силу трения, Н
-# - скорость автомобиля, м/с
-# next_mu - возвращает текущий коэффициент трения с дорогой
-# mv - масса автомобиля, кг
-# dt - время моделирования, с
-# wheel_count - количество колёс
-# g - ускорение свободного падения, м/с^2
-def vehicle(prev_v, next_mu, mv, dt = dt, wheel_count = 4, g = 9.81):
-    mpw = mv / wheel_count # масса на одно колесо
-    mu = next_mu()
-    Ff = mu * mpw * g
-    return Ff, norm(prev_v - Ff * (1 / mpw) * dt, (0, 1000))
-
-# Считает коэффициент трения с дорогой
-# s - коэффициент скольжения
-# cfs - список из 4 коэффицентов определения силы трения, которые зависят от покрытия дороги
-def friction(s, cfs):
-    s *= 100
-    mu = cfs[0] * (cfs[1] * (1 - math.exp(-cfs[2] * s)) - cfs[3] * s)
-    return norm(mu, (0, 1))
-
-# Характеристики автомобиля
-weight = 1200 # кг - масса автомобиля
-radius_wheel = 0.28 # м - радиус колеса
-inertia_wheel = 0.01 # кг*м^2 - инерция колес
+on = 1 # 1 - АБС включён, 0 - выкл.
 
 # Конфигуация АБС
-min_speed = 1.4 # м/с - минимальная скорость работы АБС
-
-# init_s - начальный коэффициент скольжения
-# init_v - начальная скорость, м/с
-# controller - контроллер АБС
-# next_s_and_v - возвращает следующие коэффициенты скольжения и скорость
-# next_k - возвращает коэффициент усиления тормозной системы
-# Tb_max - максимальный крутящий момент торможения
-# v_min - скорость, ниже которой АБС не будет работать, м/с
-# dt - время цикла моделирования, с
-def anti_lock_braking_system(init_s, init_v, controller, next_s_and_v, next_k, Tb_max, dt = dt):
-    time = 0
-    s = init_s
-    v = init_v
-    Tb = Tb_max
-    while (v > 0):
-        time += dt
-        Tb = controller(Tb, s, v, next_k, lambda v: on and v > min_speed, Tb_max, dt)
-        print("t=", time, "s")
-        print("v=", v, " m/s")
-        print("s=", s)
-        print("Tb=", Tb)
-        s, v = next_s_and_v(s, v, Tb)
-        print("-------------")
-
-eps = 1e-12 # Малое значение, чтобы избежать деления на ноль
+min_speed = 1 # Ниже этой скорости АБС не работает
 Tb_max = 2000
-init_v = 28
-w0 = init_v / radius_wheel
 
-cfs_for_beton = (0.7, 1.07, 0.5, 0.003)
-cfs_for_ice = (0.1, 1.07, 0.38, 0.007)
+# Функция-генератор
+def anti_lock_braking_system(get_v, get_ww, calc_vw, calc_slip, controller):
+    Tb = 0 # Tb - крутящий момент торможения, Н*м
 
-def next_s_and_v(prev_s, prev_v, Tb):
-    Ff, v = vehicle(prev_v, lambda: friction(prev_s, cfs_for_ice), weight, dt, 4)
+    v = get_v()
 
-    prev_ww = (1 - prev_s) * (prev_v / radius_wheel)
-    ww = wheel(prev_ww, Tb, Ff, radius_wheel, inertia_wheel)
-    print("ww=", ww)
-    print("vw=", v / radius_wheel)
-    s = slip(v / radius_wheel, lambda: ww, eps)
-    return s, v
+    while (v > 0):
+        s = 0 if not on or v < min_speed else calc_slip(get_ww(), calc_vw(v))
+        Tb = controller(s, lambda dTb: Tb + dTb)
+        yield Tb
+        v = get_v()
 
-anti_lock_braking_system(slip(w0, lambda: w0, eps), init_v, controller, next_s_and_v, lambda: 1000, Tb_max, dt)
+def get_calc(calc_slip, calc_mu, calc_Ff, calc_v, calc_ww):
+    def calc(prev_v, prev_ww, Tb, dt):
+        s = calc_slip(prev_ww, prev_v / radius_wheel)
+        mu = calc_mu(s)
+        Ff = calc_Ff(mu)
+        v = calc_v(Ff, lambda dv: prev_v + dv * dt)
+        ww = calc_ww(Tb, Ff, lambda dw: prev_ww + dw * dt)
+        return v, ww
+    return calc
+
+# Характеристики автомобиля
+weight = 1000
+radius_wheel = 0.28
+inertia_wheel = 0.01
+
+# Коэффициенты дорожных покрытий
+road_cfs = ((0.9, 1.07, 0.2773, 0.0026), (0.7, 1.07, 0.5, 0.003), (0.7, 1.07, 0.1773, 0.006), (0.1, 1.07, 0.38, 0.007))
+
+def calc_mu(road_type, slip):
+    cfs = road_cfs[road_type]
+    slip *= 100
+    return norm(cfs[0] * (cfs[1] * (1 - math.exp(-cfs[2] * slip)) - cfs[3] * slip), (0, 1))
+
+
+def calc_v(Ff, apply_dv):
+    return norm(apply_dv(-Ff / weight), (0, 1000))
+
+#сила трения
+def calc_Ff(mu):
+    return mu * weight / 4 * 9.81
+
+def calc_ww(Tb, Ff, apply_dw):
+    return norm(apply_dw(-(Tb - Ff * radius_wheel) * inertia_wheel), (0, 1000))
+
+def calc_slip(ww, vw):
+     return norm(1 - (ww / (vw + (vw == 0) * 1e-12)), (0, 1))
+
+def controller(s, k, calc_final_Tb, dt):
+    s_sign = sign(0.2 - s)
+    dTb = (s_sign * k) / (1 + s * dt)
+    return norm(calc_final_Tb(dTb * dt), (0, Tb_max))
+    
+# Сила нажатия водитель на педаль тормоза теперь зависит от времени
+def k(t):
+    return 1000 + norm(t, (0, 5)) / 5 * 1000
+
+# Тип дороги меняется от дистанции
+def get_road_type(distance):
+    return 3 if distance in range(20, 40) else 2
+
+dt = 0.1
+
+time = 0
+distance = 0
+
+v = 14
+ww = v / radius_wheel #угловая скорость колеса
+
+calc_mu_for_road = lambda s: calc_mu(get_road_type(distance), s)
+calc = get_calc(calc_slip, calc_mu_for_road, calc_Ff, calc_v, calc_ww)
+
+abs_generator = anti_lock_braking_system(
+    lambda: v, 
+    lambda: ww, 
+    lambda v: v / radius_wheel,
+    calc_slip,
+    lambda s, calc_final_Tb: controller(s, k(time), calc_final_Tb, dt))
+
+print('{:>7}{:>16}{:>16}{:>16}{:>16}{:>16}{:>16}'.format("time, s", "distance, m", "velocity, m/s", "vw, rad/s", "ww, rad/s", "slip", "Tb, N*m"))
+print(f"{time:>7.2f}{distance:>16.2f}{v:>16.4f}{v / radius_wheel:>16.2f}{ww:>16.2f}{calc_slip(ww, v / radius_wheel):>16.2f}{0:>16.2f}")
+
+for Tb in abs_generator:
+    v, ww = calc(v, ww, Tb, dt)
+    distance += v * dt
+    time += dt
+    print(f"{time:>7.2f}{distance:>16.2f}{v:>16.4f}{v / radius_wheel:>16.2f}{ww:>16.2f}{calc_slip(ww, v / radius_wheel):>16.2f}{Tb:>16.2f}")
